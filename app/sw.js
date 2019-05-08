41ca39a83953b80920f6e0f201c963add8965f97
Paulo Oliveira Marques <polivema@everis.com>
2019-05-08 15:33:27 -0300

    Finishing PWA app

--- app/sw.js
+++ app/sw.js
@@ -1,12 +1,43 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
 
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
-// Import Workbox
-// importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.2.0/workbox-sw.js');
+workbox.core.skipWaiting();
 
+workbox.core.clientsClaim();
 
-// Allow new SW to take over
-workbox.skipWaiting();
-workbox.clientsClaim();
-
-// Cache .js files - cache first
-workbox.routing.registerRoute( /\.js$/, workbox.strategies.networkFirst() );
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [
+  {
+    "url": "index.html",
+    "revision": "37c0541a0aee76e8bb4ebe770ab4732e"
+  },
+  {
+    "url": "main.js",
+    "revision": "3c275dd821735e3cf99f8118f6f2e988"
+  },
+  {
+    "url": "Classes/Camera.js",
+    "revision": "473de2d0709c0e68b99e6c7d829d7bdd"
+  },
+  {
+    "url": "Classes/Message.js",
+    "revision": "1b1b09235037cfc72d6f30fc003fb2e7"
+  }
+].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
---
+++ sw_build.js
@@ -0,0 +1,32 @@
+
+const {generateSW} = require('workbox-build');
+
+generateSW({
+
+    swDest: 'app/sw.js',
+    globDirectory: 'app',
+    globPatterns: [
+        '**/*.{html,css}',
+        'main.js',
+        'Classes/*.js'
+    ],
+    skipWaiting: true,
+    clientsClaim: true,
+
+    runtimeCaching: [
+        {
+            urlPattern: /\.(css|js)/,
+            handler: 'cacheFirst'
+        },
+        {
+            urlPattern: /^https:\/\/use\/.fontawesome\.com.*/,
+            handler: 'staleWhileRevalidate',
+            options:{
+                cacheName: 'fontawesome'
+            }
+        }
+    ]
+
+}).then(({count, size}) => {
+    console.log('Generated new service worker with ${count} precached files, totalling ${size} bytes.')
+}).catch(console.error)
